6f8a31b00b15b53adc79c66908f931431fa664e5
MarcusHagmanEcon <marcus.hagman@phdstudent.hhs.se>
2023-11-03 08:02:54 -0700

    removed what had been a comment

--- 02_R/01_scripts/03_data_merging.R
+++ 02_R/01_scripts/03_data_merging.R
@@ -29,20 +29,5 @@ gas_stations <- readRDS("01_data/02_processed/cleaned_gas_stations.rds")
 
 analysis_data <- left_join(gas_prices, gas_stations, by = c("stid" = "id"))
 
-# # Log prices
-# analysis_data <- analysis_data %>% mutate(log_e5 = log(e5),
-#                                           log_e10 = log(e10),
-#                                           log_diesel = log(diesel))
-# 
-# # Remove outliers
-# bounds <- analysis_data %>% group_by(date) %>% summarize(median = median(log_e5, na.rm = TRUE),
-#                                                       iqr = IQR(log_e5, na.rm = TRUE),
-#                                                       upper_bound_e5 = median + 5 * iqr,
-#                                                       lower_bound_e5 = median - 5 * iqr ) %>%
-#   select(date, upper_bound_e5, lower_bound_e5)
-# 
-# analysis_data <- analysis_data %>% left_join(bounds, by = c("date"))
-# 
-# analysis_data <- analysis_data %>% filter(log_e5 > lower_bound_e5 & log_e5 < upper_bound_e5)
 
 saveRDS(analysis_data, file = "01_data/02_processed/analysis_data.rds")
