0d255855aa50483e37f359152244da21389f2bea
MarcusHagmanEcon <marcus.hagman@phdstudent.hhs.se>
2023-12-06 10:57:03 -0800

    Wrote a new function to be used in the main analysis. This function takes in a regresison of a change in gas price on past changes in oil prices and calculates the cumulative response function with standard errors.

---
+++ 02_R/02_functions/cumulative_response.R
@@ -0,0 +1,17 @@
+# Calculates the cumulative response by adding up coefficient estimates
+#and estimates standard errors of the cumulative responses using the delta method
+
+# model = regression model
+# row_indices = vector of integers of elements to calculate cumulative response over
+# col_label = string for labling result
+cumulative_response <- function(model, row_indices, col_label){
+  result <- data.frame(rep(NA, length(row_indices)), rep(NA, length(row_indices)))
+  names(result) <- c(paste0(col_label, "_coef"), paste0(col_label, "_se"))
+  for (i in row_indices){
+    result[which(row_indices == i), paste0(col_label, "_coef")] <-
+      sum(model$coefficients[row_indices[1:which(row_indices == i)]])
+    result[which(row_indices == i), paste0(col_label, "_se")] <-
+      sum(vcov(model)[row_indices[1:which(row_indices == i)], row_indices[1:which(row_indices == i)]])
+  }
+  return(result)
+}
